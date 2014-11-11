301b0eda917b6ba6c0d69678a00df7be3673f5cf
Christopher Rios-Villalobos <christopher.rios-villalobos@yale.edu>
2014-11-11 16:46:11 -0500

    Phase 1 in Progress

--- Documents/workspace/CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
+++ Documents/workspace/CPSC112_Assignment3/src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
@@ -10,11 +10,11 @@ public class CPSC112_Assignment3 {
 
   public static void main(String[] args) {
     makeMySecret();
-    isGameOver("1234");
+    /*isGameOver("1234");
     isGameOver("4321");
     isGameOver("2567");
     isGameOver("1432");
-  }
+  }*/
 
   public static void makeMySecret() {
      // Part 1 code goes here (please leave the next few lines at the end of the makeMySecret method)
@@ -24,11 +24,11 @@ public class CPSC112_Assignment3 {
     }
   }
 
-  public static boolean isGuessValid(String input) {
+  /*public static boolean isGuessValid(String input) {
     // Part 2 code goes here
   }
 
   public static boolean isGameOver(String input) {
     // Parts 3 and 4 code goes here
-  }
+  }*/
 }
